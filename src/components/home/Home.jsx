d5a11a3363e79fdb067038fced22df57d9c68200
ktaller <nyanjuikinyanjui075@gmail.com>
2024-10-07 00:14:02 +0300

    work on displaying products

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Products from "../products/Products";
+// import pic from ./assets
+
+function Home() {
+  return (
+    <div className="hero">
+      <div className="card bg-success text-black border-0">
+        <img src='/src/assets/player.png' className="card-img" alt="bg" height='550px'/>
+        <div className="card-img-overlay d-flex flex-column justify-content-center">
+          <div className="container">
+          <h5 className="card-title display-3 fw-bolder mb-0">NEW SPECIAL ARRIVALS </h5>
+          <p className="card-text lead fs-2">
+            CHECK OUT TRENDS
+          </p>
+          </div>
+        </div>
+      </div>
+      <Products />
+    </div>
+  );
+}
+
+export default Home;
