f264530a4d9d58008336ce36cf110a5316f1dcc0
ktaller <nyanjuikinyanjui075@gmail.com>
2024-10-07 11:35:24 +0300

    implemented sorting the categories

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -6,7 +6,7 @@ function Home() {
   return (
     <div className="hero">
       <div className="card bg-success text-black border-0">
-        <img src='/src/assets/player.png' className="card-img" alt="bg" height='550px'/>
+        <img src='/src/assets/player.png' className="card-img" alt="background" height='550px'/>
         <div className="card-img-overlay d-flex flex-column justify-content-center">
           <div className="container">
           <h5 className="card-title display-3 fw-bolder mb-0">NEW SPECIAL ARRIVALS </h5>
--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -9,7 +9,7 @@ const Products = () => {
   useEffect(() => {
     const getProducts = async () => {
       setLoading(true);
-      const response = await fetch("https://fakestoreapi.com/products");
+      const response = await fetch("https://fakestoreapi.com/products?sort=desc");
       if (componentMounted) {
         setData(await response.clone().json());
         setFilter(await response.json());
@@ -23,23 +23,54 @@ const Products = () => {
     getProducts();
   }, []);
 
-  // const Loading = () => {
-  //   return <>Loading...</>;
-  // };
+  const Loading = () => {
+    return <>Loading...</>;
+  };
+  const FilterProducts = (cat) => {
+    const updatedList = data.filter((x)=>x.category === cat)
+    setFilter (updatedList)
+  }
+
   const ShowProducts = () => {
     return (
       <>
         <div className="buttons d-flex justify-content-center mb-5 pb-5">
-          <button className="btn btn-outline-dark me-2">All</button>
-          <button className="btn btn-outline-dark me-2">Men's Category</button>
-          <button className="btn btn-outline-dark me-2">Jewelery</button>
-          <button className="btn btn-outline-dark me-2">Electronics</button>
+          <button
+            className="btn btn-outline-dark me-2"
+            onClick={() => setFilter(data)}
+          >
+            All
+          </button>
+          <button
+            className="btn btn-outline-dark me-2"
+            onClick={() => FilterProducts("men's clothing")}
+          >
+            Men's Category
+          </button>
+          <button
+            className="btn btn-outline-dark me-2"
+            onClick={() => FilterProducts("women's clothing")}
+          >
+            Women's Clothing
+          </button>
+          <button
+            className="btn btn-outline-dark me-2"
+            onClick={() => FilterProducts("jewelery")}
+          >
+            Jewelery
+          </button>
+          <button
+            className="btn btn-outline-dark me-2"
+            onClick={() => FilterProducts("electronics")}
+          >
+            Electronics
+          </button>
         </div>
         {filter.map((product) => {
           return (
             <>
-              <div className="col-md-3 mb-4">
-                <div className="card h-100 text-center p-4" key={product.id}>
+              <div className="col-md-3 mb-2">
+                <div className="card h-70 text-center p-0" key={product.id}>
                   <img
                     src={product.image}
                     className="card-img-top"
@@ -47,12 +78,10 @@ const Products = () => {
                     height="250px"
                   />
                   <div className="card-body">
-                    <h5 className="card-title mb-0">
-                      {product.title.substring(0, 14)}...
-                    </h5>
+                    <h5 className="card-title mb-0">{product.title.substring(0,15)}...</h5>
                     <p className="card-text lead fw-bold">{product.price}</p>
                     <a href="#" className="btn btn-outline-dark">
-                      Buy Now
+                      Add to Cart
                     </a>
                   </div>
                 </div>
@@ -72,8 +101,8 @@ const Products = () => {
           </div>
         </div>
         <div className="row justify-content-center">
-          {/* {loading ? <Loading /> : */}
-          <ShowProducts />
+          {loading ? <Loading /> : <ShowProducts />}
+          
         </div>
       </div>
     </div>
